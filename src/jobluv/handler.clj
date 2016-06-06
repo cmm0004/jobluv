550bf61a380a08a377f58ab9e51c685538bd46b0
Candice Mccollough <candice.mccollough@careerbuilder.com>
2016-06-05 20:55:28 -0400

    gaze upon my works and dispair

--- src/jobluv/handler.clj
+++ src/jobluv/handler.clj
@@ -1,19 +1,46 @@
 (ns jobluv.handler
   (:require [compojure.core :refer :all]
+  			[clojure.string :as str]
   			[compojure.handler :as handler]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :refer [response]]
-            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
-(defn handle-hipchat-message [message]
-	(response message))
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [jobluv.links :as links]))
+
+(defn structure-response [usermention linkstring]
+ 	(response 
+ 		{
+	 		:color "green"
+	 		:message (format "@%s %s" usermention linkstring)
+	 		:notify false
+	 		:message_format "text"
+ 		}
+ 	)
+)
+
+(defn handle-hipchat-message [request]
+	(let [
+		command (last (str/split (get-in request [:body :item :message :message]) #" "))
+		usermention (get-in (first (get-in request [:body :item :message :mentions])) [:mention_name])
+		]
+		(case command
+			"++" (structure-response usermention (rand-nth (links/get_plus_jobluv_links)))
+			"--" (structure-response usermention (rand-nth (links/get_minus_jobluv_links)))
+			"?" (response ["i havent set up the db yet, sry"])
+			(response ["i need to know what you want"])
+		)
+	)
+)
 
 (defroutes app-routes
   (GET "/PING" [] "PONG")
-  (POST "/" {body :body} (handle-hipchat-message body))
+  (POST "/" request 
+  	(handle-hipchat-message request)
+  )	
   (route/not-found "Not Found"))
 
 (def app
   (-> (handler/api app-routes)
-   	  (wrap-json-body)
+   	  (wrap-json-body {:keywords? true})
       (wrap-json-response)))
---
+++ src/jobluv/links.clj
@@ -0,0 +1,16 @@
+(ns jobluv.links)
+
+(defn get_plus_jobluv_links []
+	'(
+	"https://i.imgur.com/OfPEnFZ.gif"
+	"https://m.popkey.co/a39a1a/7zxGZ.gif"
+	"https://m.popkey.co/ea155c/G0AgD.gif"
+	)
+)
+
+(defn get_minus_jobluv_links []
+	'(
+	"https://m.popkey.co/3060d3/DYgpr.gif"
+	"http://i.makeagif.com/media/10-12-2015/mQJF9e.gif"
+	)
+)
