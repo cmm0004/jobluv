7df55a786ebd758223a301954f93200424661432
Candice Mccollough <candice.mccollough@careerbuilder.com>
2016-06-07 07:30:16 -0400

    database and env vars

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring/ring-json "0.4.0"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [postgresql/postgresql "9.0-801.jdbc4"]]
   :plugins [[lein-ring "0.9.7"] 
             [lein-beanstalk "0.2.7"]]
   :ring {:handler jobluv.handler/app}
--- src/jobluv/handler.clj
+++ src/jobluv/handler.clj
@@ -3,10 +3,17 @@
   			[clojure.string :as str]
   			[compojure.handler :as handler]
             [compojure.route :as route]
+            [clojure.java.jdbc :as j]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :refer [response]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [jobluv.links :as links]))
+
+(def db { 	:classname "org.postgresql.Driver"
+			:subprotocol "postgresql"
+          	:subname (System/getenv "PG_SUBNAME")
+            :user (System/getenv "PG_USER")
+            :password (System/getenv "PG_PASSWORD")})
 
 (defn structure-response [usermention linkstring]
  	(response 
@@ -27,7 +34,7 @@
 		(case command
 			"++" (structure-response usermention (rand-nth (links/get_plus_jobluv_links)))
 			"--" (structure-response usermention (rand-nth (links/get_minus_jobluv_links)))
-			"?" (structure-response usermention "i havent set up the db yet, sry")
+			"?"  (prn (get-in (first (j/query db ["SELECT jobluv_amount FROM jobluvs where hipchat_username = ?" usermention]))[:jobluv_amount]))
 			(structure-response usermention "i need to know what you want")
 		)
 	)
