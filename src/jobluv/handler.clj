1acd4ff9fb192bde09b29aa93e962a636535c571
Candice Mccollough <candice.mccollough@careerbuilder.com>
2016-06-07 08:55:27 -0400

    track keys, dont listen to THEY

--- src/jobluv/handler.clj
+++ src/jobluv/handler.clj
@@ -15,6 +15,7 @@
             :user (System/getenv "PG_USER")
             :password (System/getenv "PG_PASSWORD")})
 
+
 (defn structure-response [usermention linkstring]
  	(response 
  		{
@@ -26,19 +27,76 @@
  	)
 )
 
+(defn query-jobluvs [usermention]
+	(get-in 
+		(first 
+			(j/query db ["SELECT jobluv_amount FROM jobluvs where hipchat_username = ?" usermention])
+		)
+		[:jobluv_amount]
+	)
+)
+
+(defn exec-inc-jobluvs [usermention]
+	(j/execute! db ["UPDATE jobluvs SET jobluv_amount = jobluv_amount + 1 WHERE hipchat_username = ?" usermention])
+)
+
+(defn exec-dec-jobluvs [usermention]
+	(j/execute! db ["UPDATE jobluvs SET jobluv_amount = jobluv_amount - 1 WHERE hipchat_username = ?" usermention])
+)
+
+(defn inc-dec-key [usermention should_increment]
+	(if should_increment
+		(exec-inc-jobluvs usermention)
+		(exec-dec-jobluvs usermention)
+	)
+)
+
+(defn plusplus [usermention]
+	(inc-dec-key usermention true)
+	(structure-response usermention (rand-nth (links/get_plus_jobluv_links)))
+)
+
+(defn minusminus [usermention]
+	(inc-dec-key usermention nil)
+	(structure-response usermention (rand-nth (links/get_minus_jobluv_links)))
+)
+
+(defn build-key-string [jobluv_amount]
+	(format " is major %s" (str/join " " (repeat jobluv_amount "(key)")))
+)
+
+(defn question [usermention]
+	(structure-response usermention 
+						(build-key-string 
+							(query-jobluvs usermention)
+						)
+	)
+)
+
+(defn add-user [usermention]
+	 (j/execute! db ["INSERT INTO jobluvs(hipchat_username, jobluv_amount, is_the_job_don) VALUES (?, 1, false);" usermention])		
+	 (structure-response usermention "You are now on the road to success, let's win more.")
+)
+
 (defn handle-hipchat-message [request]
 	(let [
 		command (last (str/split (get-in request [:body :item :message :message]) #" "))
 		usermention (get-in (first (get-in request [:body :item :message :mentions])) [:mention_name])
+		from (get-in (first (get-in request [:body :item :message :from])) [:mention_name])
 		]
-		(case command
-			"++" (structure-response usermention (rand-nth (links/get_plus_jobluv_links)))
-			"--" (structure-response usermention (rand-nth (links/get_minus_jobluv_links)))
-			"?"  (prn (get-in (first (j/query db ["SELECT jobluv_amount FROM jobluvs where hipchat_username = ?" usermention]))[:jobluv_amount]))
-			(structure-response usermention "i need to know what you want")
+		(if (= (System/getenv "THEY") from)
+			(structure-response usermention "I don't listen to THEY")
+			(case command
+				"++" (plusplus usermention)
+				"--" (minusminus usermention)
+				"?"  (question usermention)
+				"init" (add-user usermention)
+				(structure-response usermention "I need to know what you want")
+			)
 		)
 	)
 )
+
 
 (defroutes app-routes
   (GET "/PING" [] "PONG")
